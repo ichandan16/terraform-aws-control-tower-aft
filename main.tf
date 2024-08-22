c790c348128f3c80e59e131be9be7a2a0d639ee7
Chandan Patra <ckp1608@gmail.com>
2024-08-22 23:09:38 +0530

    all initial gitignores

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 module "aft" {
   source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
   ct_management_account_id    = var.ct_management_account_id
--- variables.tf
+++ variables.tf
@@ -1,5 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
 
 variable "ct_management_account_id" {}
 
